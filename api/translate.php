b6fbc7edc6dc65d32ab14203297f189820a3b55a
oyps <ponconsoft@gmail.com>
2023-01-13 22:35:01 +0800

    更新文档

--- api/translate.php
+++ api/translate.php
@@ -2,8 +2,21 @@
 
 /**
  * 有道翻译接口转换
+ * @author 欧阳鹏
+ * @since 2023-01-13
  */
 class Translate
 {
+    /** 来源语言代码 */
+    public string $from;
+    /** 目标语言代码 */
+    public string $to;
+    /** 待翻译的文本 */
+    public string $text;
+    public function __construct()
+    {
+    }
     
 }
+
+$translate = new Translate();
