9fcf76383e12ab7a90887f44a5a3544983482423
oyps <ponconsoft@gmail.com>
2023-01-14 16:15:07 +0800

    基础框架搭建完成

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var ponconjs_1 = __importDefault(require("ponconjs"));
+var poncon = new ponconjs_1["default"]();
+poncon.setPageList(['home', 'about']);
+poncon.start();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Poncon from 'ponconjs'
+
+const poncon = new Poncon()
+poncon.setPageList(['home', 'about'])
+poncon.start()
